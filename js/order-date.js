059855b85927d8b26c2b4db7d664752b3fc616c9
TheyCallmeOleksii <DionodeKor@gmail.com>
2024-09-12 22:53:23 +0300

    Commit

---
+++ js/order-date.js
@@ -0,0 +1,14 @@
+function updateDate() {
+  const today = new Date();
+  const day = today.getDate().toString().padStart(2, "0");
+  const month = (today.getMonth() + 1).toString().padStart(2, "0");
+  const year = today.getFullYear();
+
+  const formattedDate = `${day}-${month}-${year}`;
+  document.getElementById(
+    "date-display"
+  ).innerText = `Order date: ${formattedDate}`;
+}
+
+// Call the function to update the date when the page loads
+window.onload = updateDate;
